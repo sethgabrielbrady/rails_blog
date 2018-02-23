8577de62ca78e0fafd14813fc7aadfaba17c66db
Seth Brady <sethgabrielbrady@gmail.com>
2018-02-22 19:47:55 -0500

    updated blog app with all crud actions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  before_action :set_page, only: [:show, :edit, :update, :destroy]
 
   def index
     @pages = Page.all
@@ -9,15 +10,34 @@ class PagesController < ApplicationController
   end
 
   def show
-    @page = Page.find(params[:id])
   end
 
   def create
-    page_params = params.require(:page).permit(:title, :body, :slug)
     @page = Page.new(page_params)
     @page.save
     redirect_to @page
   end
 
+  def edit
+  end
+
+  def update
+    @page.update(page_params)
+    redirect_to @page
+  end
+
+  def destroy
+    @page.destroy
+    redirect_to pages_path
+  end
+
+  private
+    def page_params
+      params.require(:page).permit(:title, :body, :slug)
+    end
+
+    def set_page
+      @page = Page.find(params[:id])
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
-  get '/pages', to:'pages#index'
-  get '/pages/new', to:'pages#new', as: 'new_page'
-  post '/pages', to: 'pages#create'
-  get '/pages/:id', to:'pages#show', as: 'page'
+  resources :pages
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
