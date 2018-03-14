9daf0c56fe3e33f40cc023b40cfbd82513599c0e
Seth Brady <sethgabrielbrady@gmail.com>
2018-03-14 08:45:28 -0400

    rollback and updated model with references

--- db/migrate/20180314123032_add_posts_to_comments.rb
+++
@@ -1,6 +0,0 @@
-class AddPostsToComments < ActiveRecord::Migration[5.1]
-  def change
-    add_column :comments, :post_id, :integer
-    add_index :comments, :post_id
-  end
-end
--- db/migrate/20180314124358_create_comments.rb
+++ db/migrate/20180314124358_create_comments.rb
@@ -3,6 +3,7 @@ class CreateComments < ActiveRecord::Migration[5.1]
     create_table :comments do |t|
       t.text :content
       t.string :name
+      t.references :post, foreign_key: true
 
       t.timestamps
     end
